554b9841b4bb6c50825355ba22b25fd05077ecd6
vaibhav <vaibhavzade802@gmail.com>
2024-01-16 19:39:11 +0530

    adding newCollection section amd newsLetter to the shop page

---
+++ Frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./newsLetter.css"
+const NewsLetter = () => {
+  return (
+    <div className='news-letter'>
+      <h1>Get Exclusive Offer On Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div className='newsLetter_form'>
+        <input type='email' placeholder='Your Email Id'/>
+        <button>Subscribe</button>
+      </div>
+    </div>
+  )
+}
+
+export default NewsLetter
